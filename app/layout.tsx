1ba63514e04736ffde00d7cf64dd57a43c057c64
Taite Cullen <cullenta125@gmail.com>
2025-11-27 18:36:55 -0500

    fixed auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { inter } from '@/app/ui/fonts';
 import "./globals.css";
-import AuthProvider from '@/app/ui/auth-provider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        {children}
     </body>
     </html>
   );
